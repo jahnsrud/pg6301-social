ad9d545dd11342bc5dd28cc223068184ac853cd5
Markus Jahnsrud <markus@soriko.no>
2019-04-24 10:06:31 +0200

    Placeholder content on Timeline

--- src/client/components/Timeline.jsx
+++ src/client/components/Timeline.jsx
@@ -3,11 +3,27 @@ import Post from "./Post";
 
 class Timeline extends React.Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            posts: [1, 2, 3, 4, 5]
+        }
+
+    }
+
+
     render() {
         return (
             <div>
                 <h1>Timeline</h1>
-                <Post />
+                {
+                    this.state.posts.map((option) => (
+                    <Post />
+
+                    ))
+                }
+
             </div>
         )
 
